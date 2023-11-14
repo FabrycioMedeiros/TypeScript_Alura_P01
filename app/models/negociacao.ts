d78b345336f085657264a1bbd0fbb3809359208e
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-11-14 15:35:49 -0300

    Criação de arquivos para práticas do M02 do curso

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,23 +1,34 @@
 export class Negociacao {
-    //    Sintaxe utilizada em JS ES6
+    // sintaxe utilizada em TS
+   private _data;
+   private _quantidade;
+   private _valor;
+   
+//    Sintaxe utilizada em JS ES6
     // #data;
     // #quatidade;
     // #valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     get data() {
         return this._data;
     }
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return this._valor;
     }
+
     get volume() {
         return this._quantidade * this._valor;
     }
+
 }
