743b6f0775fdc416812231fbc51f691156f2be78
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-11-23 11:40:57 -0300

    Correções e ajustes para compilação do código

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,11 +1,12 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
-//import { NegociacoesViem } from "./views/negociacoes-view.js";
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    controller.adiciona();
-});
-// const negociacoesViem = new NegociacoesViem
-// const template = negociacoesViem.template();
-// console.log(template);
+if (form) {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error("Não foi possível inicializar a aplicação. Verifique se o form existe.");
+}
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,27 +1,9 @@
 export class Negociacao {
-    //sintaxe utilizada pelo TS, evita ter que declarar propriedades, evita ter que receber os parametros e atribuir para as propriedades
     constructor(_data, quantidade, valor) {
         this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
     }
-    // sintaxe utilizada em TS
-    //    private _data: Date;
-    //    private _quantidade: number;
-    //    private _valor: number;
-    //    Sintaxe utilizada em JS ES6
-    // #data;
-    // #quatidade;
-    // #valor;
-    // get data(): Date {
-    //     return this._data;
-    // }
-    // get quantidade(): number {
-    //     return this._quantidade;
-    // }
-    // get valor(): number {
-    //     return this._valor;
-    // }
     get volume() {
         return this.quantidade * this.valor;
     }
